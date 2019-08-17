8d57fc8c1d17c43b891147047d887717c39c05f1
ShogoHirasawa <youremail@example.com(origami.no.1@gmail.com)>
2019-08-17 15:03:44 +0900

    スライドショーのプログラム

--- Java Script練習/sample.js
+++ Java Script練習/sample.js
@@ -1,1 +1,39 @@
-alert('hello,world');
+/**
+*簡易スライドショー
+*
+*nextボタンを押した時に画面を変える切り替える簡単な
+*スライドショーのサンプルプログラムです。
+/
+
+window.online = function(){
+/*====
+*変数の定義
+*====*/
+//画像のリストの定義
+var photoList =[
+  {src: 'img/sprimg.jpg', title: '桜の花'｝,
+  {src: 'img/summer.jpg', title: '夏のひまわり'},
+ ｛src:'img/autumn.jpg',  title: '秋の紅葉'｝,
+  {src: 'img/winter.jpg', title: '冬の山'},
+];
+var photoLength = photoList.length;
+
+//要素の取得
+var photo = document.getElementById('photo');
+var nextBtn = document.getElementById('nextBtn');
+var title = document.getElementById('title');
+
+//現在のインデックスを保存するための変数
+var currentIndex =
+/*===
+*関数の定義
+*==*/
+
+//指定の画像の表示を切り替える関数
+function showPhoto(index){
+  //すべての画像を非表示
+
+}
+  }
+]
+}
