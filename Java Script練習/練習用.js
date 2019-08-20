cfac76fab12f6ffa5c033421653eb3a9ba708755
Shogo Hirasawa <origami.no.1@gmail.com>
2019-08-20 12:54:20 +0900

    練習用
    参考書に載ってある、練習用コードをひたすらに打ってるだけのもの。

---
+++ Java Script練習/練習用.js
@@ -0,0 +1,97 @@
+var name ='鈴木一郎'；
+console.log(mame);
+
+name = '山田太郎'；
+
+var list = ['val1' 'val2' 'val3'];
+var list =[
+    'val1',
+    'val2',
+    'val3',
+];
+
+var fruit = ['みかん', 'りんご', 'ぶどう', ];
+var orenge = fruit[0];
+var apple = fruit[1];
+var grape = fruit[2];
+
+var fruit = ['みかん', 'りんご', 'ぶどう'];
+fruit[1] = 'いちご'
+console.log(fruit);
+
+var fruit = ['みかん', 'りんご', 'ぶどう'];
+console.log(fruit.length)
+
+var person ={
+    name:'鈴木一郎',
+    age:'20',
+    gender:'male'
+};
+
+var name = person.name;
+console log(name);
+
+var person ={
+    name:'鈴木一郎',
+    age:'20',
+    gender:'male'
+    address:{
+        postcode:'100-0001'
+        pref:'東京都'
+        city:'千代田区'
+    }
+};
+var postcode = person.adderess.postcode;
+console.log(postcode);
+
+var person ={
+    name:'鈴木一郎',
+    age:'20',
+    gender:'male'
+  };
+var key ='name';
+var name = 'person[name]';
+console.log(name);
+
+var name = '鈴木一郎'；
+console.log(name === '鈴木一郎')；
+
+if (age >=20){
+  console.log('年齢は20歳以上です')；
+}
+
+var perspn ={
+  name:'鈴木一郎'
+  age:20
+  gender:'male'
+  url:null
+};
+var name = '鈴木一郎'
+if (name === '鈴木一郎')｛
+  console.log('鈴木一郎です')
+  ｝
+
+if (x > 100){
+  console.log('xが100より大きいです')；
+} else if (x>50){
+  console.log('xが100以下かつ50より大きい値です')；
+}　else {
+  console.log('xが50以下の値です')；
+}
+
+var i = 0;
+while (i<10){
+  console.log(i);
+  i++;
+}
+
+for (var = 0; i < 10; i++){
+  console.log(i)
+}
+
+var fruit = ['みかん', 'りんご', 'ぶどう']；
+var value;
+for (var i = 0; i < 3; i ++){
+    value = fruit[i];
+    console.log(value)
+}
